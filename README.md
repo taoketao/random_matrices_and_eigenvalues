81ee7a86b992c98029432e948b46bb10e21a79a5
taoketao <morgan.r.bryant@gmail.com>
2021-08-11 03:21:35 -0700

    quick-synced :wq Wed Aug 11 03:20:56 2021 more content, and freshened up readme a smidge

--- README.md
+++ README.md
@@ -3,31 +3,37 @@ Scripts, experiments, results, and reports on questions surrounding spectra of r
 
 **below is draft material. please dont quote me on it! I need to fact-check several claims**
 
-Recurrent neural network attractors have the potential to act as state systems, automata, and turing-complete computers. Therefore, a better understanding of these attractors could shed light on how neural networks in a cortex might possibly do complex computation.
+Recurrent neural network attractors have the potential to act as state systems, automata, and turing-complete computers. Therefore, a better understanding of these attractors could shed light on how neural networks in a cortex might possibly implement complex computation. Their fundamentally distributed nature also begs the question of the extent to which state systems can be distributed while still retaining computational expressiveness. First we seek to understand the basic properties of these attractors, such as How many are there, When during learning to they arise, Are they the same shape, and How difficult is it to move an instant set of activations from one attractor to a different?
 
-Recurrent neural network connections can be modeled by a graph or connectivity matrix. Repeated activations over time correspond to repeated application of a matrix operator, up to nonlinear activation functions. This scenario can be understood with tools from dynamical systems theory involving stationary distributions. Verily, the term 'attractor' comes from dynamical systems, referring to states that a consistent system tends towards over time. As it turns out, the attractors given by a matrix correspond directly to its eigenvector spectrum. If an eigenvector has eigenvalue equal to 1, then it is an attractor. ((todo: insert latex inline proof)) For random strictly stochastic matrices, there is exactly one eigenvector with eigenvalue 1 (with all other eigenvectors admitting smaller eigenvalues) so there is one attractor. So by analyzing the eigenspectra of neural network connectivity matrices, we can begin to understand the true capabilities these attractors. 
+These initial experiments study the shape and numerosity of attractors of a fully-learned RNN via eigenspectra. Recurrent neural network connections can be modeled by a graph or connectivity matrix; repeated application of a matrix operator correspond to recurrent activations over time. This scenario can be analyzed with tools from dynamical systems theory involving stationary distributions and ergodic theory. Verily, the term 'attractor' comes from dynamical systems, referring to states that a consistent system tends towards over time. As it turns out, the attractors given by a matrix correspond directly to its eigenvector spectrum. If an eigenvector has eigenvalue equal to 1, then it is an attractor. ((todo: insert latex inline proof)) For random strictly stochastic matrices, there is exactly one eigenvector with eigenvalue 1 (with all other eigenvectors admitting smaller eigenvalues) so there is one attractor. So by analyzing the eigenspectra of neural network connectivity matrices, we can begin to understand the true capabilities these attractors. 
 
-In this code, I've modeled a few kinds of connectivity matrices, each with specific purposes. Then, I analyzed the spectra of these matrices. The (absolute) strength of the the eigenvalues correspond to the basin size of an attractor, roughly speaking ((todo fix this up)), so the first analysis was geared towards looking at how quickly the eigenvalues decreased. The first matrices I analyzed were:
+In the code in script.py, I've modeled different kinds of connectivity matrices, each with specific purposes. Then, I analyzed the spectra of these matrices. The (absolute) strength of the the eigenvalues correspond to the basin size of an attractor, roughly speaking ((todo fix this up)), so the first analysis was geared towards looking at how quickly the eigenvalues decreased. The first matrices I analyzed were:
  - Binary matrices, where each element of the NxN matrix is drawn ~ Ber(0.5). This is a simple connectivity matrix. Its biological interpretation is that some neurons connect to each other and some don't in a way that seems random to us, but this is mostly as a sanity check.
  - Uniform matrices taking on real values between 0 and 1, "Unif(0,1)". It is also used as a point of comparison / sanity check.
  - Ternary matrices, with elements taking on values -1, 0, and 1 with equal probability. This is similar to a Hopfield network. Understanding the spectra of these matrices were a core part of this computer experiment.
- 
- others implemented for plot #3:
  - uniform(-1,1)
  - uniform discrete of [0,0.5,1,1.5,2,2.5,3]
  - normal, mean 0, stdev 1
  - normal, mean 0.2, stdev 1
  - normal, mean -0.2, stdev 1
  - two-humped normal, norm(.2)+norm(-.2)
+ - hopfield ternary, which is a ternary but with diagonals zeroed out
+ - orthonormal from the orthogonal group
+ - orthonormal with gaussian noise stutter
+ - Identity
  
 
  not yet implemented:
- - Uniform matrices taking on real values between -1 and 1, "Unif(-1,1)"
- - Small integer matrices.
+ - binary {-1,1} matrices
  - Hopfield network-like binary matrices, which are like binary random matrices except the diagonals are zero
- - Hopfield network-like ternary matrices, which are like ternary random matrices except the diagonals are zero
+ - nearly orthonormal matrices 
+ - partially orthogonal matrices
 
-
-For each type of matrix, I sampled many random instances, computed the eigenvalues, sorted them, and took the average (and stdev) over the instances. See plots binary_ternary_uniform_01.png and ####XYZXYZ####. It's well known in dynamical systems theory that binary random matrices tend to have one large eigenvalue near unity and the remaining eigenvalues are about the same at value much less than one. The plots show as much: As the size of the square matrix increases, the binary matrix indeed seems to achieve one large eigenvaue and many smaller ones at a similar place. ((todo: are the remaining ones normally distributed?)). It also seems to be the case for Unif(0,1) matrices. But interestingly for ternary matrices, the eigenvalues degrade smoothly. This contrasts with the intuition we have about mode-learning of neural networks thanks to Saxe, Ganguli, and McClelland, 2013, "Exact Solutions...": we'd expect that networks would tend to learn one orthonormal eigenmode(?) at a time in correspondence with the only-one-large-eigenvalue.
+For each type of matrix, I sampled many random instances, computed the eigenvalues, sorted them, and took the average (and stdev) over the instances. For plots made before 02:50 8.11, I used absolute value of eigenvalues, and after that, used singular values ie squared eigenvalues. See plots binary_ternary_uniform_01.png and ####XYZXYZ####. It's well known in dynamical systems theory that binary random matrices tend to have one large eigenvalue near unity and the remaining eigenvalues are about the same at value much less than one. The plots show as much: As the size of the square matrix increases, the binary matrix indeed seems to achieve one large eigenvaue and many smaller ones at a similar place. ((todo: are the remaining ones normally distributed?)). It also seems to be the case for Unif(0,1) matrices. But interestingly for ternary matrices, the eigenvalues degrade smoothly. This contrasts with the intuition we have about mode-learning of neural networks thanks to Saxe, Ganguli, and McClelland, 2013, "Exact Solutions...": we'd expect that networks would tend to learn one orthonormal eigenmode(?) at a time in correspondence with the only-one-large-eigenvalue.
 
 anyways, there's math behind all this, and these experiments are for intuition mostly. Enjoy the script!
+
+
+
+
+, up to nonlinear activation functions
--- script.py
+++ script.py
@@ -1,73 +1,135 @@
+''' See README.md '''
 import numpy as np
 import numpy.linalg as la
 from time import sleep, ctime
 import matplotlib as mpl
 from matplotlib import pyplot as plt
+from scipy.stats import ortho_group
 
-''' -------------------- utilities -------------------- '''
-def Round(x, decimals=4):
-    x = (10**decimals * x).astype(int)/10**decimals
-    return x
+''' -------------------- Settings and parameters -------------------- '''
 
-''' -------------------- settings -------------------- '''
-Scaling=7; Decimals=4; Iterations=10; Sleep=0.03; Printing=False; epsilon=1e-10
-PlotStdev=False
-number_matrix_types = 9
+#using_number_matrix_types = 5
+special_selections = None # [2,5,6,7,8]
+special_selections = [0,1,2,3,4,5,6,7,8,9]#2,5,8,10,11,12]
+square_matrix_sizes=[4,8,12,20,35,50,100,350]  # the columns in plot
+square_matrix_sizes=[4,8,15,35,50,100,200]  # the columns in plot
 
+Iterations=20; 
 
-''' -------------------- parameters -------------------- '''
-square_matrix_sizes=[4,6,8,12,25,50,100,350]
-square_matrix_sizes=[4,8,12,25,100,350]
+Decimals=8; 
 
-def getmatr0(): return np.random.randint(0,2,((Iterations,MatrixSize,MatrixSize)))
-matr0kind, matr0kind_full = 'binary {0,1}', 'Ber(0.5) binary with probs={0:1/2, 1:1/2}'
-def getmatr1(): return np.random.uniform(0,1,((Iterations,MatrixSize,MatrixSize)))
-matr1kind, matr1kind_full = 'uniform(0,1)', 'Uniform(0,1) with uniform probability on the unit interval'
-def getmatr2(): return np.random.randint(-1,2,((Iterations,MatrixSize,MatrixSize)))
-matr2kind, matr2kind_full = 'ternary {-1,0,1}', 'Ternary with probs={-1:1/3, 0:1/3, 1:1/3}'
+PlotStdev=True
 
-def getmatr3(): return np.random.uniform(-1,1,((Iterations,MatrixSize,MatrixSize)))
-matr3kind, matr3kind_full = 'uniform(-1,1)', 'Uniform(-1,1) with uniform probability on the unit interval'
-def getmatr4(): return np.random.randint(0,7,((Iterations,MatrixSize,MatrixSize)))/3
-matr4kind, matr4kind_full = 'integers(0,6)/3', 'Discrete uniform with probs=1/7 for integers 0..6/3'
-def getmatr5(): return np.random.normal(0,1,((Iterations,MatrixSize,MatrixSize)))
-matr5kind, matr5kind_full = 'normal', 'normal, 0 mean 1 std'
-shift=0.1
-def getmatr6(): return np.random.normal(shift,1,((Iterations,MatrixSize,MatrixSize)))
-matr6kind, matr6kind_full = str(shift)+'-shiftnormal', 'normal, '+str(shift)+' mean 1 std'
-shift=-0.1
-def getmatr7(): return np.random.normal(shift,1,((Iterations,MatrixSize,MatrixSize)))
-matr7kind, matr7kind_full = str(shift)+'-shiftnormal', 'normal, '+str(shift)+' mean 1 std'
-def getmatr8(): return np.random.normal(0.2,1,((Iterations,MatrixSize,MatrixSize))) + np.random.normal(-0.2,1,((Iterations,MatrixSize,MatrixSize)))
-matr8kind, matr8kind_full = 'twohump', 'two-hump, normal(0.2,1)+normal(-0.2,1)'
-
-def get_matrices(): return [getmatr0(), getmatr1(), getmatr2(), getmatr3(), getmatr4(), \
-        getmatr5(), getmatr6(), getmatr7(), getmatr8()][:number_matrix_types]
-def get_matrix_info_brief(): return [matr0kind,matr1kind,matr2kind,matr3kind,matr4kind,\
-        matr5kind, matr6kind, matr7kind, matr8kind][:number_matrix_types]
-def get_matrix_info_full(): return [matr0kind_full,matr1kind_full,matr2kind_full,\
-        matr3kind_full,matr4kind_full,matr5kind_full,matr6kind_full,matr7kind_full,matr8kind_full\
-        ][:number_matrix_types]
+Sleep=0.03; 
+Printing=False; 
+epsilon=1e-10
+epsilon_for_ortho=1e-3
+Scaling=7; 
 
 
-''' -------------------- overhead -------------------- '''
+''' -------------------- Matrices and custom distributions -------------------- 
+    Users, feel welcome to edit here. The source_matrices dictionary holds 
+    information about how to construct a matrix that you care about. To 
+    make your own matrices, format your entry as a dictionary key-value
+    pair where the key is a unique id and the value is a 3-long array:
+    (1) a functional that makes a matrix of shape/size 'm_sz' when called, 
+    (2) a short name, (3) a full name.'''
+#
+#def callback_9_hopfield_ternary(m_sz):  # python !== javascript
+#    x = np.random.randint(-1,2,m_sz) 
+#    x.multiply( 1-np.identity
+##    np.fill_diagonal(x,0, axis1=1, axis2=2)
+#    return x
+
+source_matrices = {
+    -101:[lambda m_sz: np.array([[1,2,3,4],[5,6,7,8]]),
+        'example 1!', 'Example 1!'],
+    -102:[lambda m_sz: np.array(np.arange(np.prod(m_sz))).reshape(m_sz),
+        'example 2!', 'Example 2!'],
+    0: [lambda m_sz: np.random.randint(0,2,m_sz), 
+        'binary {0,1}', 
+        'Ber(0.5) binary with probs={0:1/2, 1:1/2}'],
+    1: [lambda m_sz: np.random.uniform(0,1,m_sz), 
+        'uniform(0,1)', 
+        'Uniform(0,1) with uniform probability on the unit interval'],
+    2: [lambda m_sz: np.random.randint(-1,2,m_sz),
+        'ternary {-1,0,1}', 
+        'Ternary with probs={-1:1/3, 0:1/3, 1:1/3}'],
+    3: [lambda m_sz: np.random.uniform(-1,1,m_sz),
+        'uniform(-1,1)', 
+        'Uniform(-1,1) with uniform probability on the unit interval'],
+    4: [lambda m_sz: np.random.randint(0,7,m_sz)/3,
+        'integers(0,6)/3', 
+        'Discrete uniform with probs=1/7 for integers 0..6/3'],
+    5: [lambda m_sz: np.random.normal(0,1,m_sz),
+        'normal', 
+        'normal, 0 mean 1 std'],
+    6: (lambda shift: 
+         [lambda m_sz: np.random.normal(shift,1,m_sz), 
+         str(shift)+'-shiftnormal', 
+         'normal, '+str(shift)+' mean & 1 std']
+         )(0.1),
+    7: (lambda shift: 
+         [lambda m_sz: np.random.normal(shift,1,m_sz), 
+         str(shift)+'-shiftnormal', 
+         'normal, '+str(shift)+' mean & 1 std']
+         )(-0.1),
+    8: [lambda m_sz: np.random.normal(0.2,1,m_sz) + np.random.normal(-0.2,1,m_sz), 
+        'twohump', 
+        'two-hump, normal(0.2,1)+normal(-0.2,1)'],
+    9: [lambda m_sz: np.multiply(np.random.randint(-1,2,m_sz), 1-np.identity(m_sz[1])),
+        'hopfield ternary',
+        'ternary with diagonals zeroed, as in hopfield networks'],
+    10: [lambda m_sz: np.stack([f() for f in [lambda:ortho_group.rvs(m_sz[1])]*m_sz[0]]),
+        'ortho',
+        'orthogonal random vectors of length 1'],
+    11: [lambda m_sz: np.stack([f() for f in [lambda:ortho_group.rvs(m_sz[1])]*m_sz[0]])+np.random.normal(0,.1,m_sz),
+        'ortho_stutter',
+        'orthogonal random vectors of length 1, stuttered gaussian'],
+    12: [lambda m_sz: np.array([np.identity(m_sz[1])]*m_sz[0]),
+        'identity',
+        'identity matrix'],
+}
+
+if not 'using_number_matrix_types' in locals():
+    if special_selections: using_number_matrix_types=len(special_selections)
+    else:
+        using_number_matrix_types = len([k for k in source_matrices.keys() if k>=0])
+assert( len(source_matrices)>=using_number_matrix_types )
+assert( (not special_selections) or using_number_matrix_types<=len(special_selections))
+
+def _get_src_matr(matr_id, matr_size): 
+    return source_matrices[matr_id][0]( (Iterations, matr_size, matr_size) )
+def get_matrices(matr_size, select_matrices=range(using_number_matrix_types)): 
+    return [_get_src_matr(matr_id, matr_size) for matr_id in select_matrices]
+def get_matrix_info_brief(select_matrices=range(using_number_matrix_types)): 
+    return [source_matrices[matr_id][1] for matr_id in select_matrices]
+def get_matrix_info_full(select_matrices=range(using_number_matrix_types)): 
+    return [source_matrices[matr_id][2] for matr_id in select_matrices]
+
+
+''' -------------------- overhead and utilities-------------------- '''
 means, stdevs = [],[]
 if PlotStdev==True:
     number_of_statistics = 2
 else:
     number_of_statistics = 1
-plot_height = number_matrix_types * number_of_statistics 
+plot_height = using_number_matrix_types * number_of_statistics 
+def Round(x, decimals=4):
+    x = (10**decimals * x).astype(int)/10**decimals
+    return x
 
-''' -------------------- setup, populate, and beautify Matplotlib plots -------------------- '''
-fig, axs = plt.subplots(plot_height , len(square_matrix_sizes), figsize=(14, 7)) 
+''' -------------------- Matplotlib setup, populate, and beautify -------------------- '''
+fig, axs = plt.subplots(plot_height , len(square_matrix_sizes), figsize=(14, 20)) #figsize=(14, 7)) 
 mpl.rcParams.update({'font.size': 6})
-if number_of_statistics==2: _s = "Eigenvalue means are sorted by absolute value are in red and corresponding standard deviations are in blue. "
-else: _s = ''
+_s = "Eigenvalue means are sorted by absolute value are in red "+\
+         "and corresponding standard deviations are in blue. " if number_of_statistics==2 else ''
+full_infos = get_matrix_info_full() if not special_selections else get_matrix_info_full(special_selections)
 fig.suptitle("The absolute eigenvalues of random matrices, averaged over "+str(Iterations)+\
-        " iterations. \nRows: "+';  '.join(get_matrix_info_full()[:4])+ \
-        "; \n"+';  '.join(get_matrix_info_full()[4:])+\
+        " iterations. \nRows: "+';  '.join(full_infos[:4])+ \
+        "; \n"+';  '.join(full_infos[4:])+\
         " all equally sampled. \n"+_s+"Columns: "\
-        +str(square_matrix_sizes)+"-sized square matrices. "+ctime())
+        +str(square_matrix_sizes)+"-sized square matrices. "+ctime()+" PST")
 # subplot labels
 for nth_matrix, MatrixSize in enumerate(square_matrix_sizes): 
     axs[0, nth_matrix].set_title(str(MatrixSize)+'x'+str(MatrixSize)+' matrix')
@@ -75,37 +137,55 @@ for nth_matrix, MatrixSize in enumerate(square_matrix_sizes):
     for r in range(0,plot_height-1):
         axs[r,nth_matrix].set_xticks([],[])
 
-for distr_kind_iter, distribution_kind in enumerate( get_matrix_info_brief()):
+brief_infos = get_matrix_info_brief() if not special_selections else get_matrix_info_brief(special_selections)
+for distr_kind_iter, distribution_kind in enumerate( brief_infos ):
     axs[0+distr_kind_iter*number_of_statistics ,0].set_ylabel(\
                     'mean \n'+distribution_kind, rotation=0, labelpad=20)
     if number_of_statistics==2:
         axs[1+distr_kind_iter*number_of_statistics,0].set_ylabel(\
                     'stdev\n'+distribution_kind, rotation=0, labelpad=20)
-
-#axs[0,0].set_ylabel('mean \n'+,      rotation=0, labelpad=20)
-#axs[1,0].set_ylabel('stdev\nbinary {0,1}',      rotation=0, labelpad=20)
-#axs[2,0].set_ylabel('mean \nternary {-1,0,1}',  rotation=0, labelpad=20)
-#axs[3,0].set_ylabel('stdev\nternary {-1,0,1}',  rotation=0, labelpad=20)
-#axs[4,0].set_ylabel('mean \nuniform(0,1)',      rotation=0, labelpad=20)
-#axs[5,0].set_ylabel('stdev\nuniform(0,1)',      rotation=0, labelpad=20)
+        for nth_matrix in range(len(square_matrix_sizes)):
+            continue
+            axs[1+distr_kind_iter*number_of_statistics,nth_matrix].set_ylim(0,1)
 
 
 ''' -------------------- computations -------------------- '''
 for nth_matrix, MatrixSize in enumerate(square_matrix_sizes):
-    random_matrices = get_matrices()
-    for mi_, matr in enumerate(random_matrices):
+    if special_selections: random_matrices = get_matrices(MatrixSize, special_selections)
+    else: random_matrices = get_matrices(MatrixSize)
+    for matr_ind, matr in enumerate(random_matrices):
         eigs = la.eigvals(matr)
-        eigs = abs(eigs)
-        eigs = Round(eigs, decimals=Decimals)
-        eigs.sort(1)
-        eigs = np.fliplr(eigs)
-        means += [Round(eigs.mean(0))]
-        stdevs += [Round(np.std(eigs,0))]
+#        eigs = np.multiply(eigs, eigs) # ie, singular values
+#        eigs = abs(eigs) 
+#        eigs = Round(eigs, decimals=Decimals)
+#        eigs.sort(1)
+#        eigs = np.fliplr(eigs)
+#        means += [Round(eigs.mean(0))]
+#        stdevs += [Round(np.std(eigs,0))]
+
+        u,sings,vt = la.svd(matr)
+        sings = Round(sings, decimals=Decimals)
+        sings.sort(1)
+        sings = np.fliplr(sings)
+        means += [Round(sings.mean(0))]
+        stdevs += [Round(np.std(sings,0))]
+
 
         marker='.' if MatrixSize<=25 else ''
-        axs[0+number_of_statistics*mi_, nth_matrix].plot( means[-1], marker=marker, c='red')
+        cur_ax = axs[0+number_of_statistics*matr_ind, nth_matrix]
+        cur_ax.plot( means[-1], marker=marker, c='red')
+
+        # numerical correction for orthonormal cases
+#        print(cur_ax.get_ylim()[0]-cur_ax.get_ylim()[1], epsilon_for_ortho)
+        if abs(cur_ax.get_ylim()[0]-cur_ax.get_ylim()[1])<epsilon_for_ortho:
+                cur_ax.set_ylim((means[-1][-1]+means[-1][0])/2-epsilon_for_ortho, \
+                                (means[-1][-1]+means[-1][0])/2+epsilon_for_ortho)
+#                print( '--', means[-1], (means[-1][-1]+means[-1][0])/2-epsilon_for_ortho, \
+#                        (means[-1][-1]+means[-1][0])/2+epsilon_for_ortho)
+
         if number_of_statistics==2:
-            axs[1+number_of_statistics*mi_, nth_matrix].plot( stdevs[-1], marker=marker, c='blue')
+            cur_ax=axs[1+number_of_statistics*matr_ind, nth_matrix]
+            cur_ax.plot( stdevs[-1], marker=marker, c='blue')
 
         if Printing:
             print(eigs)
@@ -117,5 +197,6 @@ for nth_matrix, MatrixSize in enumerate(square_matrix_sizes):
             print('stdev:', Round(np.std(eigs,0)))
             input()
 
-plt.show()
+#plt.show()
+plt.savefig('./assorted_plots_06.png')
 plt.close()
